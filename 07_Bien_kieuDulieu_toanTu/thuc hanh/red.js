cb0783e3088f7013bfff19afd4e4b700c5c8e398
unknown <thuythuy100897@gmail.com>
2021-04-02 11:10:55 +0700

    commit bai thuc hanh 7

---
+++ 07_Bien_kieuDulieu_toanTu/thuc hanh/red.js
@@ -0,0 +1,10 @@
+let inputWidth = prompt("Enter the width");
+let inputHeight = prompt("Enter the height");
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("Enter width: </br>" + width);
+document.write("</br>")
+document.write("Enter height: </br>" + height);
+document.write("</br>")
+document.write("The area is: </br>" + area);
